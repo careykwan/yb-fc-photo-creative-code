0aafa926cb764c06fb2bb00cf8553ad2bd17f7c6
Forrest Wilson <forrest.wilson@me.com>
2017-11-02 14:50:01 +1300

    Working slideshow

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
       $('#slideshow').slick({});
+
+      $(".image-wrapper").click(function() {
+            $("#slideshowWrapper").fadeIn(500);
+      });
+
+      $("#closeIcon").click(function() {
+            $("#slideshowWrapper").fadeOut(500);
+      });
 });
