182d6f031fb022492c3668977b76bc6cc24801ef
Forrest Wilson <forrest.wilson@me.com>
2017-11-03 13:15:07 +1300

    Slideshow popup showing correct slide

--- js/app.js
+++ js/app.js
@@ -2,13 +2,28 @@ $(document).ready(function() {
       $('#slideshow').slick();
 
       $(".image-wrapper").click(function() {
+
+            var $imageArray = $(".image-wrapper");
+
+            for (i = 0; i < $imageArray.length; i++) {
+                  if ($imageArray[i]) {
+                        $($imageArray[i]).data("idx", i);
+                  }
+            }
+
+            var index = $(this).data("idx");
+
+            console.log(index);
+
             $("#slideshowWrapper").css("opacity", "0");
 
             $("#slideshow").slick("unslick");
 
             $("#slideshowWrapper").css("display", "block");
 
-            $('#slideshow').slick();
+            $('#slideshow').slick({
+                  initialSlide: index
+            });
 
             $("#slideshowWrapper").animate({
                   "opacity": "1"
